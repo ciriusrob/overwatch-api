a5d44eff56286da76631041f8e461830cc60694b
Robert Wilson <robert.wilson@minex360.com>
2019-08-28 00:25:31 +0200

    Initial commit

---
+++ src/main/java/com/robertwilson/overwatchapi/OverWatchApiApplication.java
@@ -0,0 +1,15 @@
+package com.robertwilson.overwatchapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OverWatchApiApplication
+{
+
+    public static void main( String[] args )
+    {
+        SpringApplication.run(OverWatchApiApplication.class, args);
+    }
+
+}
